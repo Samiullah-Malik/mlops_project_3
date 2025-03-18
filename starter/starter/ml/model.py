f4d127c8eb5df16a0d8543b2a7120516a086f26b
Samiullah Malik <samiullah.malik@richmond.edu>
2025-03-18 16:55:52 +0100

    Complete train_model and inference methods

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,4 +1,7 @@
+import numpy as np
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 # Optional: implement hyperparameter tuning.
@@ -18,7 +21,13 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    # Define the model (Random Forest)
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+    # Train the model
+    model.fit(X_train, y_train)
+
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +66,7 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    # Run inference (predict)
+    preds = model.predict(X)
+
+    return np.array(preds)
